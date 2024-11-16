cbf0f2cf107183e9e606c5ca360d72f551b23a0f
geniobrillian <geniobrillian88@gmail.com>
2024-11-16 19:52:36 +0700

    add tugas 8

---
+++ Minggu-ke8/switch.php
@@ -0,0 +1,31 @@
+<?php
+$nomor = 1;
+
+switch ($nomor) {
+    case 1:
+        echo "Senin";
+        break;
+    case 2:
+        echo "Selasa";
+        break;
+    case 3:
+        echo "Rabu";
+        break;
+    case 4:
+        echo "Kamis";
+        break;
+    case 5:
+        echo "Jumat";
+        break;
+    case 6:
+        echo "Sabtu";
+        break;
+    case 7:
+        echo "Minggu";
+        break;
+    default:
+        echo "Masukkan nomor yang sesuai";
+        break;
+}
+
+?>
