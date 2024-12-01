d40515a7f5ac2a689d1edb140821c561dfbc679a
geniobrillian <geniobrillian88@gmail.com>
2024-12-01 11:26:29 +0700

    add percobaan dowhile and foreach

---
+++ Minggu-ke10/dowhile.php
@@ -0,0 +1,7 @@
+<?php
+$a = 10;
+
+do {
+    echo "ini adalah perulangan while ke-$a<br>";
+    $a--;
+} while ($a > 0);
---
+++ Minggu-ke10/foreach.php
@@ -0,0 +1,13 @@
+<?php
+$books = [
+    "buku a",
+    "buku b",
+    "buku c",
+    "buku d",
+];
+
+echo "<ul>";
+foreach($books as $buku){
+    echo "<li>$buku</li>";
+}
+echo "</ul>";
