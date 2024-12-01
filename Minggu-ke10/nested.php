50fef31ec76345e951ed8bf99b2e871dc1713e6e
geniobrillian <geniobrillian88@gmail.com>
2024-12-01 11:33:29 +0700

    add percobaan nested loop

---
+++ Minggu-ke10/nested.php
@@ -0,0 +1,15 @@
+<?php 
+for ($i=1; $i <=5 ; $i++) { 
+    for ($j=1; $j <=5 ; $j++) { 
+        echo "ini adalah perulangan ke ($i, $j)<br>";
+    }
+}
+
+echo "<br>";
+$a = 0;
+while ($a <= 6) {
+    for ($b=0; $b <=10 ; $b++) { 
+        echo "ini adalah perulangan ke ($a, $b)<br>";
+    }
+    $a++;
+}
