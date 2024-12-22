c5a05b466b051ee195b406c35cc689403cedca82
geniobrillian <geniobrillian88@gmail.com>
2024-12-22 20:31:24 +0700

    rename title

--- Minggu-ke13/session/index.php
+++ Minggu-ke13/session/index.php
@@ -32,7 +32,7 @@ if (isset($_POST['submit'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="style.css">
-    <title>Niagahoster Tutorial</title>
+    <title>Session Tutorial</title>
 </head>
 <body>
     <div class="container">
