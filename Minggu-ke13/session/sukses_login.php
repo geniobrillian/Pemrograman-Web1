94540d55a50344939ed890f2a4fe620c1a69e942
geniobrillian <geniobrillian88@gmail.com>
2024-12-22 20:30:58 +0700

    modif session

--- Minggu-ke13/session/sukses_login.php
+++ Minggu-ke13/session/sukses_login.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if (isset($_SESSION["username"])){
+if ( !isset($_SESSION["username"])){
     header("Location: index.php");
     exit();
 }
@@ -20,7 +20,7 @@ if (isset($_SESSION["username"])){
         <form action="logout.php" method="POST" class="login-email">
             <h1>Selamat Datang, <?php echo $_SESSION['username'];?></h1>
             <div class="input-group">
-                <button type="submit" class="btn">logout</button>
+                <button type="submit" class="btn">logout</button>                
             </div>
         </form>
     </div>
